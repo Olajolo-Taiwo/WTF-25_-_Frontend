5dacb56bf8cd8a4060d79ad8135ef7d3d236f1a4
Timilehin <olajolotaiwo2@gmail.com>
2025-09-22 13:11:33 +0100

    Add basic arithmetic operations and output results

---
+++ script.js
@@ -0,0 +1,12 @@
+let num1 = 25;
+let num2 = 100;
+let name = "odette";
+let sum = num1 + num2;
+let subtraction = num2 - num1
+let multiplication = num1 * num2
+let division = num2/num1
+document .getElementById("message").innerText = "Hello "  + name +  ", the results of num1 + num2 is " + sum + ".";
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+console.log(name)
